a5bdf4aad8262a64ae862062ac5a42466a2040ac
jinwoong <ultraman1128@gmail.com>
2016-09-12 12:12:44 -0400

    Added javascript files for Twitter API

---
+++ Node_js/config.js
@@ -0,0 +1,7 @@
+
+module.exports = {
+  consumer_key:         '3g0fedYWhFu0VW3EjzlUJQvY3',
+  consumer_secret:      'EQxYqmV4yz7JnPYaBuQJ8PfJPBeh3jqNq54p3gayBTcku5CZEm',
+  access_token:         '125165204-dWDqS67p5fWA7SAWabN2XW29vcEa1IuulzSsJPK7',
+  access_token_secret:  'O4jRWcAFMPjmH6FVRBKdNV15lSgHCXk8GsoM3aGnHR3I6'
+}
