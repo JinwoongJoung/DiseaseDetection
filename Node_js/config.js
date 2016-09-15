a2e054e9a6a23791a35c807ffcbe4b935af1b25a
jinwoong <ultraman1128@gmail.com>
2016-09-14 20:25:58 -0400

    Edited Javascript files

--- Node_js/config.js
+++ Node_js/config.js
@@ -1,7 +1,7 @@
 
 module.exports = {
-  consumer_key:         '3g0fedYWhFu0VW3EjzlUJQvY3',
-  consumer_secret:      'EQxYqmV4yz7JnPYaBuQJ8PfJPBeh3jqNq54p3gayBTcku5CZEm',
-  access_token:         '125165204-dWDqS67p5fWA7SAWabN2XW29vcEa1IuulzSsJPK7',
-  access_token_secret:  'O4jRWcAFMPjmH6FVRBKdNV15lSgHCXk8GsoM3aGnHR3I6'
+  consumer_key:         '9VUQs9mJvWm0IrsYItq144lqB',
+  consumer_secret:      'tYBDQFtQ1RBQ2SCC8MYA84TAKySsmr4mQ1xmDteLPMv5ygIG6n',
+  access_token:         '125165204-N1ZfWabfEtBV0d9FcfV6bqlr2xY21nDLXlfkFMrO',
+  access_token_secret:  'aMSicIcQJvkNUl0JKVsfAQVw8npyZS0bl5jjZXoPjeaAt'
 }
