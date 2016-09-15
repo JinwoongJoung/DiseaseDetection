e5727e1b6019321846971b191281bcb38413c5b2
jinwoong <ultraman1128@gmail.com>
2016-09-14 20:28:39 -0400

    deleted unused files

--- Node_js/config.js
+++
@@ -1,7 +0,0 @@
-
-module.exports = {
-  consumer_key:         '9VUQs9mJvWm0IrsYItq144lqB',
-  consumer_secret:      'tYBDQFtQ1RBQ2SCC8MYA84TAKySsmr4mQ1xmDteLPMv5ygIG6n',
-  access_token:         '125165204-N1ZfWabfEtBV0d9FcfV6bqlr2xY21nDLXlfkFMrO',
-  access_token_secret:  'aMSicIcQJvkNUl0JKVsfAQVw8npyZS0bl5jjZXoPjeaAt'
-}
