cac66240f20877b13e7a922beaa5a19f5e962779
jinwoong <ultraman1128@gmail.com>
2016-09-15 22:16:37 -0400

    Added Streaming API example

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 
 module.exports = {
-  consumer_key:         '9VUQs9mJvWm0IrsYItq144lqB',
-  consumer_secret:      'tYBDQFtQ1RBQ2SCC8MYA84TAKySsmr4mQ1xmDteLPMv5ygIG6n',
-  access_token:         '125165204-N1ZfWabfEtBV0d9FcfV6bqlr2xY21nDLXlfkFMrO',
-  access_token_secret:  'aMSicIcQJvkNUl0JKVsfAQVw8npyZS0bl5jjZXoPjeaAt'
+  consumer_key:         'Pr36jmrM7h14TzNtMUlnyCDGo',
+  consumer_secret:      'jpLWE7RAcha0PGBFv5Z0AN08uUedtr1TBuOGofc9G9iASHifwr',
+  access_token:         '125165204-26S8cXw2LBAdKjRyP0hZd3URbZlgxaktH5UzCXtv',
+  access_token_secret:  '9waydt5j1UgW6MQBKZROyPgC3AUrLZEFOoMdMKW4ag8Bl'
 }
